a127b47f05666675aab8ab06db86bfecfc97717a
enbeze93 <65225312+enbeze93@users.noreply.github.com>
2024-05-24 13:14:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "files" {
   source       = "./modules/files"
   file_count   = 3
   file_prefix  = "example"
-  file_content = "This is an example file."
+  file_content = "Something something"
 }
 
 output "created_filenames" {
