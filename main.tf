7da19f8465a0ec7d762e0b631453ef4bfba236af
enbeze93 <65225312+enbeze93@users.noreply.github.com>
2024-05-24 13:20:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,15 @@ module "files" {
   file_content = "Something something"
 }
 
+module "read" {
+  source    = "./modules/read"
+  filenames = module.files.filenames
+}
+
 output "created_filenames" {
   value = module.files.filenames
+}
+
+output "read_prefixed_filenames" {
+  value = module.read.prefixed_filenames
 }
