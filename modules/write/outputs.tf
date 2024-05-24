5ead74ff04083d4644e37068ded3283da40f0a88
enbeze93 <65225312+enbeze93@users.noreply.github.com>
2024-05-24 13:22:34 +0200

    Create outputs.tf

---
+++ modules/write/outputs.tf
@@ -0,0 +1,27 @@
+output "answer_1" {
+  value = var.answer_1
+}
+
+output "answer_2" {
+  value = var.answer_2
+}
+
+output "answer_3" {
+  value = var.answer_3
+}
+
+output "answer_4" {
+  value = var.answer_4
+}
+
+output "answer_5" {
+  value = var.answer_5
+}
+
+output "answer_6" {
+  value = var.answer_6
+}
+
+output "answer_7" {
+  value = var.answer_7
+}
