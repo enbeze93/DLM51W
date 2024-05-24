caf9b7f05509cb3afc4412cb49edddbf06c433cd
enbeze93 <65225312+enbeze93@users.noreply.github.com>
2024-05-24 13:25:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,33 @@ module "read" {
   filenames = module.files.filenames
 }
 
+module "write" {
+  source   = "./modules/write"
+  answer_1 = var.answer_1
+  answer_2 = var.answer_2
+  answer_3 = var.answer_3
+  answer_4 = var.answer_4
+  answer_5 = var.answer_5
+  answer_6 = var.answer_6
+  answer_7 = var.answer_7
+}
+
 output "created_filenames" {
   value = module.files.filenames
 }
 
 output "read_prefixed_filenames" {
   value = module.read.prefixed_filenames
+}
+
+output "answers" {
+  value = {
+    answer_1 = module.write.answer_1
+    answer_2 = module.write.answer_2
+    answer_3 = module.write.answer_3
+    answer_4 = module.write.answer_4
+    answer_5 = module.write.answer_5
+    answer_6 = module.write.answer_6
+    answer_7 = module.write.answer_7
+  }
 }
