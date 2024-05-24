c9fdf43974e78a6b63834d25409cc7a5086e0b17
enbeze93 <65225312+enbeze93@users.noreply.github.com>
2024-05-24 13:28:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,41 @@ module "read" {
   filenames = module.files.filenames
 }
 
+variable "answer_1" {
+  description = "Answer to question 1"
+  type        = string
+}
+
+variable "answer_2" {
+  description = "Answer to question 2"
+  type        = string
+}
+
+variable "answer_3" {
+  description = "Answer to question 3"
+  type        = string
+}
+
+variable "answer_4" {
+  description = "Answer to question 4"
+  type        = string
+}
+
+variable "answer_5" {
+  description = "Answer to question 5"
+  type        = string
+}
+
+variable "answer_6" {
+  description = "Answer to question 6"
+  type        = string
+}
+
+variable "answer_7" {
+  description = "Answer to question 7"
+  type        = string
+}
+
 module "write" {
   source   = "./modules/write"
   answer_1 = var.answer_1
